893b72a432882b785683103bc3bc9040bdb69900
alok kumar0421 <aloknokha01@gmail.com>
2026-02-20 22:50:24 +0530

    add new

--- 04-Card-project/src/App.jsx
+++ 04-Card-project/src/App.jsx
@@ -4,13 +4,15 @@ import Card from "./components/Card"
 export const App = () => {
   return (
     <>
+    <div className='full'>
     <div className="parent">
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Card link="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8WU0Emt19dyXiCPIuhVtxIFbqx13mkj54hA&s" brand="Amazon" price="$120/hr"/>
+    <Card link="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/500px-Google_%22G%22_logo.svg.png" brand="Google" price="$150-120k/hr"/>
+    <Card link="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zdLpQ1XogOHnpR9MuW1MD9oXYxuPS5FCgQ&s" brand="Triddle" price="$85/hr"/>
+    <Card link="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSljqaDsIkzZQsQ01cpqsnbJ4TF75ZfXhVHQg&s" brand="Figma" price="$70/hr"/>
+    <Card link="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSicMDzk5BVKCMaspR4wow1fC8Kt9fDI-ACBg&s" brand="AirBnb" price="$120-90k/hr"/>
+    <Card link="https://1000logos.net/wp-content/uploads/2016/10/Apple-Logo.png" brand="Apple" price="$120/hr"/>
+    </div>
     </div>
     </>
   )
--- 04-Card-project/src/components/Card.jsx
+++ 04-Card-project/src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {Bookmark} from 'lucide-react'
-const Card = () => {
+const Card = (props) => {
   return (
       <div className="card">
         <div className="top">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8WU0Emt19dyXiCPIuhVtxIFbqx13mkj54hA&s" alt="amazon img" />
+          <img src={props.link} alt="amazon img" />
           <button>Save <Bookmark size={20}/></button>
         </div>
         
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
+          <h3>{props.brand} <span>5 days ago</span></h3>
           <h2>Senior UI/UX Designer</h2>
           <div className='tag'>
             <h6>Part-Time</h6>
@@ -18,7 +18,7 @@ const Card = () => {
         </div>
         <div className="bottom">
           <div className='details'>
-              <h3>$120/hr</h3>
+              <h3>{props.price}</h3>
               <p>Mumbai, India</p>
           </div>
           <button>Apply Now</button>
