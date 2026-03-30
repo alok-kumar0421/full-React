69b1d3c92768236505bc8675f076cc7484344538
alok kumar0421 <aloknokha01@gmail.com>
2026-03-30 15:23:56 +0530

    useeffect

---
+++ 15-useEffect/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [num,  setNum] = useState(0)
+  const [num2,  setNum2] = useState(0)
+
+  useEffect(function(){
+    console.log("pritting.........")
+  },[num2])  // if num2 will change then it will change other wise not
+  return (
+    <div>
+      <h2>{num}</h2>
+      <h2>{num2}</h2>
+      <button onClick={()=>{
+        setNum(num+1)
+      }}
+      onDoubleClick={()=>{
+        setNum2(num2+1)
+      }}>click me</button>
+      
+    </div>
+  )
+}
+
+export default App
